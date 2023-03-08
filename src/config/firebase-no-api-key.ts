adb3ab6cff04271a76abbb6fe1f220bf79775ca3
Jusiah Sioson <71131010+jusiahjusiah@users.noreply.github.com>
2023-03-08 19:14:46 +0800

    commit firebase config with no api-key

---
+++ src/config/firebase-no-api-key.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+import {getAuth, GoogleAuthProvider } from "firebase/auth"
+//importing firestore to access database
+import { getFirestore } from 'firebase/firestore'
+const firebaseConfig = {
+
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const provider = new GoogleAuthProvider();
+//tells our app that we are going to use fireStore
+export const db = getFirestore(app)
